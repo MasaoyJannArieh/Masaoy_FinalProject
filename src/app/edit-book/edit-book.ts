e150f8a4978adc0b387aafe316c66982faf38ad3
masaoy_j <masaoy_j@f112.ub.lan>
2026-07-02 09:55:37 +0800

    feat(edit-book): read selected book ID and return to book list after update

--- src/app/edit-book/edit-book.ts
+++ src/app/edit-book/edit-book.ts
@@ -6,6 +6,7 @@ import {
   ReactiveFormsModule,
   Validators
 } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-book',
@@ -16,9 +17,16 @@ import {
 })
 export class EditBookComponent {
 
-  bookForm!: FormGroup;
+  bookForm: FormGroup;
+  bookId!: number;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+
+    this.bookId = Number(this.route.snapshot.paramMap.get('id'));
 
     this.bookForm = this.fb.group({
       title: ['Atomic Habits', Validators.required],
@@ -39,10 +47,12 @@ export class EditBookComponent {
       return;
     }
 
+    console.log('Book ID:', this.bookId);
     console.log(this.bookForm.value);
 
     alert('Book updated successfully!');
 
+    this.router.navigate(['/books']);
   }
 
 }
